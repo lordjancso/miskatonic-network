093c53955cc62b144dbfe87d381fdf5562a19255
lordjancso <lordjancso@gmail.com>
2018-08-28 08:46:37 +0200

    Added draggable demo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,32 @@
 require('bootstrap');
+
+import draggable from 'vuedraggable';
+import Vue from 'vue';
+
+new Vue({
+    el: '#app',
+
+    data() {
+        return {
+            drag: false,
+            myArray: [
+                {
+                    name: 'első',
+                    src: '/images/cards/cthuloid-spawn-som.jpg'
+                },
+                {
+                    name: 'második',
+                    src: '/images/cards/shudde-mell-tgs.jpg'
+                },
+                {
+                    name: 'harmadik',
+                    src: '/images/cards/a-fortunate-accident-er.jpg'
+                },
+            ]
+        };
+    },
+
+    components: {
+        draggable
+    }
+});
